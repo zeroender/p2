5428538731fe62914f992f67722ac0b3ef2faea6
Brian Worthington <worthington.brian@gmail.com>
2017-09-17 19:24:31 -0400

    Initial sample index page

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Scheduling Assistant</title>
+    <!-- Head code excluded for brevity -->
+</head>
+<body>
+
+    <h1>Enter the days of the week you are available for work</h1>
+
+    <p>
+        This is a sample html index page
+    </p>
+
+</body>
+</html>
